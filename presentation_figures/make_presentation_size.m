f71c34ddd0aab31f178ccd11b1016597d2c927b8
Turner Silverthorne <turner.silverthorne@gmail.com>
2022-08-17 00:19:09 -0400

    More careful use of backslash in modified Wynn's method

--- presentation_figures/make_presentation_size.m
+++ presentation_figures/make_presentation_size.m
@@ -1,13 +1,14 @@
 close all
 fig_list={'atwood_design.fig','hist_sim_result.fig','linear_regression_histogram.fig', ...
           'modified_wynn.fig','nonlinear_regression_histogram_top.fig','samplegrid_fig.fig', ...
-          'simulated_annealing_best.fig','wynn_convergence.fig'}
-fig_list{6}
+          'simulated_annealing_best.fig','wynn_convergence.fig'};
+fig_list={'modified_wynn_correct.fig','modified_wynn_incorrect.fig'};
+%fig_list{6}
 %%
-for ii=[6]
+for ii=1:numel(fig_list)
     openfig(fig_list{ii})
     f=gcf;
-    set(findall(gcf,'-property','FontSize'),'FontSize',45)
+    set(findall(gcf,'-property','FontSize'),'FontSize',45/5)
 %     chil = get(gcf,'children');
 %     h=chil(2).Children;
 %     for i = 1:length(h)
@@ -17,21 +18,21 @@ for ii=[6]
 %     end
     lines = findobj(gcf,'Type','ConstantLine');
     for i = 1:numel(lines)
-        lines(i).LineWidth = 5;
+        lines(i).LineWidth = 5/5;
     end
     lines = findobj(gcf,'Type','line');
     for i = 1:numel(lines)
-        lines(i).LineWidth = 5;
-        lines(i).MarkerSize = 25;
+        lines(i).LineWidth = 5/5;
+        lines(i).MarkerSize = 25/5;
     end
     lines = findobj(gcf,'Type','scatter');
     for i = 1:numel(lines)
-        lines(i).SizeData = 800;
+        lines(i).SizeData = 800/5;
     end
     plot_filename=strcat('pres_version_',fig_list{ii}(1:end-4));
    
-    wd=6*5; % width
-    ht=3*5; % height
+    wd=6; % width
+    ht=3/2; % height
     set(gcf,'PaperUnits','inches')
     set(gcf,'PaperPositionMode','manual','PaperSize',[wd,ht],'PaperPosition',[0 0 wd ht])
     print(gcf,plot_filename,'-dpng','-r600') % -r sets the resolution
