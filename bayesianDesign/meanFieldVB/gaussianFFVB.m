afdda9478603ccc36294e6344285a2e60f57f75f
Turner Silverthorne <turner.silverthorne@gmail.com>
2022-12-03 17:22:51 -0500

    Minor

--- bayesianDesign/meanFieldVB/gaussianFFVB.m
+++ bayesianDesign/meanFieldVB/gaussianFFVB.m
@@ -3,7 +3,7 @@
 % simplify(solve(N-d*(d+1)/2,d))
 % f=@(N) (8*N + 1)^(1/2)/2 - 1/2
 % f()
-clear all
+clear 
 % GPU random variable generation
 NS=1e7;% number of samples
 d=3;
@@ -12,13 +12,11 @@ Ngauss=3;
 Lvec = rand(d*(d+1)/2,Ngauss);
 muvec = rand(d,Ngauss);
 wvec = rand(Ngauss,1); wvec = wvec/sum(wvec);
+fprintf('running\n')
+gputimeit(@() generateSamples(muvec,Lvec,wvec,NS,d,Ngauss,true))
+timeit(@() generateSamples(muvec,Lvec,wvec,NS,d,Ngauss,false))
+function S=generateSamples(muvec,Lvec,wvec,NS,d,Ngauss,useGPU)
 
-tic
-isgpuarray(generateSamples(muvec,Lvec,wvec,NS,d,Ngauss))
-toc
-
-function S=generateSamples(muvec,Lvec,wvec,NS,d,Ngauss)
-useGPU=true;
 switch useGPU
     case true
         eps  = randn(d,1,NS,'gpuArray');
@@ -30,27 +28,16 @@ end
 
 avec = cumsum(wvec);
 avecm1= [0; avec(1:end-1)];
-xi=avecm1<u & u<avec ;
-
-S=(muvec(:,1)+pagemtimes(ivh(Lvec(:,1)),eps) ).*xi(1,:,:);
-
-if Ngauss>1
-    S=S+(muvec(:,2)+pagemtimes(ivh(Lvec(:,2)),eps) ).*xi(2,:,:);
-end
-if Ngauss>2
-    S=S+(muvec(:,3)+pagemtimes(ivh(Lvec(:,3)),eps) ).*xi(3,:,:);
-end
-if Ngauss>3
-    S=S+(muvec(:,4)+pagemtimes(ivh(Lvec(:,4)),eps) ).*xi(4,:,:);
-end
-if Ngauss>4
-    S=S+(muvec(:,5)+pagemtimes(ivh(Lvec(:,5)),eps) ).*xi(5,:,:);
-end
-if Ngauss>5
-    S=S+(muvec(:,6)+pagemtimes(ivh(Lvec(:,6)),eps) ).*xi(6,:,:);
-end
-if Ngauss >6
-    print("stop: too many gaussians")
+xi=avecm1<u & u<avec;
+counts=sum(xi,2);
+S=NaN(d,NS,'gpuArray');
+cc=cumsum(counts);
+Lmatnow=ivh(Lvec(:,1));
+S(:,1:cc(1))=muvec(:,1) + Lmatnow*eps(:,1:cc(1));
+for ii=2:Ngauss
+    i0=cc(ii-1)+1;
+    Lmatnow=ivh(Lvec(:,ii));
+    S(:,i0:cc(ii))=muvec(:,ii) + Lmatnow*eps(:,i0:cc(ii));
 end
 end
    
