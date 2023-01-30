acfddbf4d5edb9b4ea3cd7885b3d53a37143ec5f
Turner Silverthorne <turner.silverthorne@gmail.com>
2023-01-30 15:01:01 -0500

    Parallel optimization code for one stage experiment

--- power/checkUseGPU.m
+++ power/checkUseGPU.m
@@ -8,6 +8,10 @@ switch simtype
         param.Nperm=1e2;
         param.Nresidual=1e2; % SMALL RIGHT NOW
         param.Nacro=32; % num. fourier samples
+    case 'medium' 
+        param.Nperm=1e2;
+        param.Nresidual=1e3; % SMALL RIGHT NOW
+        param.Nacro=32; % num. fourier samples
     case 'long'
         param.Nperm=1e3;
         param.Nresidual=1e3; % SMALL RIGHT NOW
--- power/oneStageExpt.m
+++ power/oneStageExpt.m
@@ -1,11 +1,12 @@
 clear
 clf
-simtype='rough';
+tic
+simtype='medium';
 checkUseGPU
 tic
 param.NL=4;
 param.NR=4;
-param.freq_true=2.5; % freq used in regression model
+param.freq_true=1.5; % freq used in regression model
 param.Amp=1;
 param.noise1=1;
 param.noise2=1; % the noise actually used in the simulation
@@ -13,13 +14,14 @@ nodes='uniform';
 hold on
 ylim([0,1])
 
-noisevals=logspace(-1,1,3);
+noisevals=logspace(log10(2),log10(1/2),4);
 costfun=@(t) -min(simulatePWR(param,cumsum(abs(t))/sum(abs(t))));
-swarm_opts=optimoptions(@particleswarm,'Display','Iter','MaxIterations',1);
+swarm_opts=optimoptions(@particleswarm,'Display','Iter','MaxIterations',5,'UseParallel',true);
 acrovec=linspace(0,2*pi,param.Nacro);
 Nfreq=1;
 tiledlayout(2,Nfreq,'TileSpacing','tight','Padding','tight')
 
+cvals=linspace(0.8,0,length(noisevals))'.*[1 1 1];
 fprintf('running\n')
 for ii=1:length(noisevals)
     fprintf('%d\n',ii)
@@ -32,8 +34,13 @@ for ii=1:length(noisevals)
     [topt,fval]=particleswarm(costfun,param.NL+param.NR,[],[],swarm_opts);
     p_nu=simulatePWR(param,topt);
     nexttile(1)
-    plot(acrovec,p_nu)
+    plot(acrovec,p_nu,'Color',cvals(ii,:))
+    ylim([0,1])
+    hold on
     nexttile(Nfreq+1)
-    plot(acrovec,p_nu-p_unif)
+    ylim([-1,1])
+    plot(acrovec,p_nu-p_unif,'Color',cvals(ii,:))
+    hold on
     drawnow
 end
+toc
