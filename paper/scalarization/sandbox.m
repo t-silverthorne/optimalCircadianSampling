28f88b9588c04614a69a80e72d520ae0e148c6e0
Turner Silverthorne <turner.silverthorne@gmail.com>
2023-05-09 19:30:11 -0400

    Added simulated annealing

--- paper/scalarization/sandbox.m
+++ paper/scalarization/sandbox.m
@@ -2,9 +2,9 @@
 active_inds = 1:5;
 
 nouter = 10; % number of outer optimzation loops
-ninner = 20; % number of inner optimization loops
-parpool_size=15;Nbatch=5;popu_size=30;
-
+ninner = 100; % number of inner optimization loops
+parpool_size=15;Nbatch=5;popu_size=30;fname='scalarized_test_simulanneal.mat';
+parpool(parpool_size)
 numOpt = length(active_inds);    % dimension of multi-objective optimization problem
 % construct weight matrix
 addpath('../utils_core/')
@@ -21,9 +21,9 @@ lambdaMaster
 
 p.permMethod= 'naive_reuse_perms';% p.permActionMethod = 'index'; % options index or matrix for 'naive_make_perms_first'
 p.Nmeas     = 8;
-p.Nacro     = 32;
+p.Nacro     = 16;
 p.Nresidual = 1e3;
-p.Nperm     = 2e2;
+p.Nperm     = 1e2;
 p.noise     = 1;
 p.Nbatch    = Nbatch;
 p.Amp       = 2;
@@ -35,32 +35,45 @@ eps_cstr=1e-2;
 xmaster=NaN(nouter,p.Nmeas-1);
 
 %%
-for ii=1:nouter
-    % construct swarm matrix
-    A=rand(popu_size,p.Nmeas-1);
-    SM=A./(.1*rand(popu_size,1)+sum(A,2));
-    
-    % optimization options
-    opts = optimoptions(@particleswarm,'Display','iter','SwarmSize', popu_size, ...
-            'UseParallel',true,'MaxIterations',ninner,'InitialSwarmMatrix',SM);
-    xopt = particleswarm(@(dt) hvol_scalar_costfun(dt,p,active_inds,lambdaMaster(ii,:)),p.Nmeas-1,eps_cstr*ones(1,p.Nmeas-1),ones(1,p.Nmeas-1)/p.Nmeas,opts);
-    
+optmethod='simulannealbnd';
+parfor ii=1:nouter
+
+    switch optmethod
+        case 'particleswarm'
+            % construct swarm matrix
+            A=rand(popu_size,p.Nmeas-1);
+            SM=A./(.1*rand(popu_size,1)+sum(A,2));
+            
+            % optimization options
+            opts = optimoptions(@particleswarm,'Display','iter','SwarmSize', popu_size, ...
+                    'UseParallel',true,'MaxIterations',ninner, ...
+                    'InitialSwarmMatrix',SM);
+            xopt = particleswarm(@(dt) hvol_scalar_costfun(dt,p,active_inds,lambdaMaster(ii,:)), ...
+                    p.Nmeas-1,eps_cstr*ones(1,p.Nmeas-1),ones(1,p.Nmeas-1)/p.Nmeas,opts);
+        case 'simulannealbnd'
+            opts = optimoptions(@simulannealbnd,'Display','iter', ...
+                    'MaxIterations',ninner,'DisplayInterval',1,'ReannealInterval',15);
+            x0=rand(1,p.Nmeas-1);
+            xopt = simulannealbnd(@(dt) hvol_scalar_costfun(dt,p,active_inds,lambdaMaster(ii,:)), ...
+                                       x0/(.1*rand+sum(x0)),eps_cstr*ones(1,p.Nmeas-1),ones(1,p.Nmeas-1)/p.Nmeas,opts);
+    end
     xmaster(ii,:)=xopt;
 end
 
-save('scalar_testrun.mat')
-%%%
-%disp("finished1")
-%clf
-%for ii=1:nouter
+
+save(fname)
+%%
+% disp("finished1")
+% clf
+% for ii=1:nouter
 %    t=[0 cumsum(xmaster(ii,:))];
 %    J=wrap_getCostFun(t,p,active_inds)
 %    plot(J(4),J(5),'ok')
 %    hold on
-%end
-%disp("finished2")
-
-
+% end
+% disp("finished2")
+% 
+% 
 
 
 
