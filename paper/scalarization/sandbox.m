7bc97de107cf5c9668730850d6e6ff6039cc947b
Turner Silverthorne <turner.silverthorne@gmail.com>
2023-05-10 15:30:09 -0400

    New time normalization

--- paper/scalarization/sandbox.m
+++ paper/scalarization/sandbox.m
@@ -1,25 +1,28 @@
 % generate weights
 active_inds = 1:5;
 
-nouter = 120; % number of outer optimzation loops
-ninner = 100; % number of inner optimization loops
-parpool_size=30;Nbatch=5;popu_size=30;fname='scalarized_test_simulanneal_larger_batch.mat';
+nouter = 120;                    % number of outer optimzation loops
+ninner = 100;                    % number of inner optimization loops
+parpool_size=30;
+Nbatch=5;
+popu_size=30;
+fname='scalarized_test_simulanneal_larger_batch.mat';
 parpool(parpool_size)
 numOpt = length(active_inds);    % dimension of multi-objective optimization problem
 % construct weight matrix
 addpath('../utils_core/')
 addpath('../utils_cost_fun/')
 
-lambdaMaster=[];
+
+lambdaMaster=[];                 % Sample positive orthant of n sphere
 while size(lambdaMaster,1)<nouter
     lambdaMat=randn(nouter,numOpt);
     lambdaMat=lambdaMat./sqrt(sum(lambdaMat.^2,2));
     lambdaMaster = [lambdaMaster; lambdaMat(sum(lambdaMat>0,2)==numOpt,:)];
 end
-lambdaMaster
-% run particle swarm
 
-p.permMethod= 'naive_reuse_perms';% p.permActionMethod = 'index'; % options index or matrix for 'naive_make_perms_first'
+%% run particle swarm
+p.permMethod= 'naive_reuse_perms';
 p.Nmeas     = 8;
 p.Nacro     = 16;
 p.Nresidual = 1e3;
@@ -34,7 +37,6 @@ eps_cstr=1e-2;
 
 xmaster=NaN(nouter,p.Nmeas-1);
 
-%%
 optmethod='simulannealbnd';
 parfor ii=1:nouter
 
@@ -55,7 +57,7 @@ parfor ii=1:nouter
                     'MaxIterations',ninner,'DisplayInterval',1,'ReannealInterval',15);
             x0=rand(1,p.Nmeas-1);
             xopt = simulannealbnd(@(dt) hvol_scalar_costfun(dt,p,active_inds,lambdaMaster(ii,:)), ...
-                                       x0/(.1*rand+sum(x0)),eps_cstr*ones(1,p.Nmeas-1),ones(1,p.Nmeas-1)/p.Nmeas,opts);
+                                       x0,eps_cstr*ones(1,p.Nmeas-1),ones(1,p.Nmeas-1)/p.Nmeas,opts);
     end
     xmaster(ii,:)=xopt;
 end
@@ -118,20 +120,13 @@ end
 
 disp("finished2")
 
-% 
-
-
-
-
-
-
-
-
-
-
 
 
 function J=hvol_scalar_costfun(dt,p,active_inds,lambdaloc)
-t=[0 cumsum(dt)];
+t=NaN(1,length(dt)+1);
+t(1)=0;
+for ii=2:length(t)
+    t(ii) = t(ii-1) + t(ii)*(1-t(ii-1));
+end
 J=max(wrap_getCostFun(t,p,active_inds)./lambdaloc);
 end
