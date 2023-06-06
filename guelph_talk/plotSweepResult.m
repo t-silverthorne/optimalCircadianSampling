5c242e1e08ae580865356111e4857f59a133bb15
t-silverthorne <turner.silverthorne@gmail.com>
2023-06-06 17:10:55 -0400

    minor

--- guelph_talk/plotSweepResult.m
+++ guelph_talk/plotSweepResult.m
@@ -1,6 +1,6 @@
 % path and data
 close all
-tiledlayout(1,2)
+
 fig=gcf
 
 load('data/sweepNvals_test.mat')
@@ -11,8 +11,8 @@ addpath('utils_cost_fun')
 set(groot,'defaultAxesTickLabelInterpreter','latex');  
 set(groot,'defaulttextinterpreter','latex');
 set(groot,'defaultLegendInterpreter','latex');
-fig_width = 10; % width in inches
-fig_height = 5; % height in inches
+fig_width = 5; % width in inches
+fig_height = 3; % height in inches
 fig_aspect_ratio = fig_width / fig_height;
 set(fig, 'PaperUnits', 'inches', ...
          'PaperSize', [fig_width fig_height], ...
@@ -21,10 +21,10 @@ set(fig, 'PaperUnits', 'inches', ...
          'Units', 'inches', ...
          'Position', [0 0 fig_width fig_height]);
 
-%%
-clf
 
-nexttile(1)
+clf
+tiledlayout(2,2,'TileSpacing','tight')
+nexttile(1,[2 1])
 [M,c]=contourf(Agr,Fgr,Pmin,100,'LineColor','none');%,'FaceAlpha',0.5)
 set(gca,'XScale','log')
 
@@ -32,15 +32,15 @@ clim manual
 clim([0,1])
 
 cb = colorbar;
-cb.Layout.Tile = 'south';
+cb.Layout.Tile = 'east';
 cb.Label.Interpreter='latex'
-cb.Label.String='$\textrm{min}_\phi \gamma(\phi)$';
+cb.Label.String='$\textrm{min}_\phi \gamma(\phi;A,f)$';
 set(cb,'TickLabelInterpreter','latex')
 cb.Label.Interpreter='latex'
 
-p.Nbatch=1;
-p.Amp=3.5938;
-p.freq=3 ;
+p.Nbatch = 1;
+p.Amp    = 2;
+p.freq   = 4;
 
 testing=false;
 if testing
@@ -55,8 +55,11 @@ else
 
 end
 
-xline(p.Amp,'-w')
-yline(p.freq,'-w')
+xl=xline(p.Amp,'--k','LineWidth',2,'color','white');
+yl=yline(p.freq,'--k','LineWidth',2,'color','white');
+
+xl.Color(4)=1;
+yl.Color(4)=1;
 
 [~,indA]=min(abs(Agr(1,:)-p.Amp));
 [~,indf]=min(abs(Fgr(:,1)-p.freq));
@@ -64,10 +67,41 @@ yline(p.freq,'-w')
 
 nexttile(2)
 [pwr,~,~]=getPowerBatch(t_unif,p)
-pwr=mean(pwr,1)
+pwr=mean(pwr,1);
 acrovec=linspace(0,2*pi,p.Nacro+1);
 acrovec=acrovec(1:end-1); % get acrophases
 plot(acrovec,reshape(pwr,[1 p.Nacro 1 1]),'-ok')
 hold on
 yline(Pmin(indf,indA),'--k')
 drawnow
+
+
+nexttile(1)
+xlabel('amplitude')
+ylabel('frequency')
+
+nexttile(2)
+xlabel('acrophase','Interpreter','latex')
+ylabel('power','Interpreter','latex')
+
+ylim([0,1])
+xlim([0 2*pi])
+xticks([0 pi/2 pi 3*pi/2 2*pi])
+xticklabels({'$$0$$','$$\frac{\pi}{2}$$','$$\pi$$','$$\frac{3\pi}{2}$$','$$2\pi$$'})
+xline(pi/2,'linewidth',2,'color',"#648FFF")
+
+%%
+nexttile(4)
+tv=0:.01:1;
+oscf=@(t) p.Amp*cos(2*pi*p.freq*t-pi/2);
+plot(tv,oscf(tv),'-k','LineWidth',1)
+hold on
+plot(t_unif,oscf(t_unif),'.k','MarkerSize',20,'Color',"#648FFF")
+xlabel('$t$')
+ylabel('$\cos(2\pi f t - \pi/2)$','Interpreter','latex')
+
+
+
+print(gcf,'outputfigs/sweepNvals.png','-dpng','-r600') % -r sets the resolution
+
+
