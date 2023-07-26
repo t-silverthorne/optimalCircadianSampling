1b042d588acf53e8b27bb3f7b8d9a6146f1744b6
Turner Silverthorne <turner.silverthorne@gmail.com>
2023-07-26 17:18:13 -0400

    Construct design matrix using Julia

---
+++ main/jump_solver/sandbox.jl
@@ -0,0 +1,34 @@
+#===============
+# Functions
+===============#
+
+function constructX(t,p)
+# Construct the design matrix 
+    x0=ones(length(t),1);
+    x1=sin.(2*pi*p.freq*t);
+    x2=cos.(2*pi*p.freq*t);
+    return [x0 x1 x2];
+end
+
+function constructReducedX(t,p)
+# Construct the design matrix assuming beta0=0 
+    x1=sin.(2*pi*p.freq*t);
+    x2=cos.(2*pi*p.freq*t);
+    return [x1 x2];
+end
+
+Base.@kwdef mutable struct harmonic_param
+# store parameters for harmonic regression 
+    Amp::Float64=1;
+    freq::Float64=1;
+    phase::Float64=0;
+end
+
+#===============
+# Main program 
+===============#
+p=harmonic_param();
+t=[0:.1:1;];
+t=t[1:end-1];
+
+X=constructX(t,p);
