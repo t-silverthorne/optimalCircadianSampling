1db7d37cf64a0e33656c4ecdf15a57e0f9a3f260
Turner Silverthorne <turner.silverthorne@gmail.com>
2023-08-01 18:56:57 -0400

    Rewrote matlab plot in Julia

--- main/jump_solver/plot_example.jl
+++ main/jump_solver/plot_example.jl
@@ -1,43 +1,120 @@
-using Base: namemap
 using Plots, Plots.Measures 
 using LaTeXStrings
 include("utils.jl")
-#%# random plot example
 plot_font = "Computer Modern"
 default(
     fontfamily=plot_font,
     linewidth=2, 
     framestyle=:box, 
     label=nothing, 
-    grid=false
+    grid=false,
+    titlefontsize=5,
+    guidefontsize=5,
+    tickfontsize=5,
+    legendfontsize=5,
 )
-
-x=1:0.5:20;
-y=1:0.5:10;
-f(x,y)=begin
-  (3x+y^2)*abs(sin(x)+cos(y));
-end
-X=repeat(reshape(x,1,:),length(y),1);
-Y=repeat(y,1,length(x));
-Z=map(f,X,Y);
-levels=0:10:200
-p1=contourf(x,y,f,fill=true,color=:turbo,cbar=:right,
-        titlefontsize=12,
-        guidefontsize=12,
-        tickfontsize=12
-)
-plot!(size=(800,600))
-savefig("test.pdf")
-#%# Actual plot
+##################################################
+#%# Fig 2 top: heatmap for uniform and non-uniform
 N=8
-namp  = 10;
-nfreq = 10;
+namp  = 2^6;
+nfreq = 2^6;
 
 t     = LinRange(0,1,N+1)
 t     = t[1:end-1]
-lamp  = LinRange(-2,2,namp);
+lamp  = LinRange(-2,log10(3),namp);
 amp   = [10^i for i in lamp];
 freq  = LinRange(1,16,nfreq) 
 f(amp,freq) = getMinPower(t,amp,freq)
 
-contourf(amp,freq,f,fill=true,color=:turbo,linewidth=0)
+
+# left panel
+plt1=heatmap(amp,freq,f,clim=(0,1),levels=10,fill=true,color=:viridis,linewidth=0,
+       xlabel=L"$A$", ylabel=L"$f$")
+plot!(colorbar=false)
+
+
+# right panel
+t=randn(length(t))
+plt2=heatmap(amp,freq,f,clim=(0,1),levels=10,fill=true,color=:viridis,linewidth=0,
+       xlabel=L"$A$")
+plot!(yticks=false)
+plot!(colorbar=false,location=:none)
+
+
+# dummy plot for adding colorbar
+h2 = scatter([NaN NaN], [NaN NaN], zcolor=NaN, clims=(0,1),
+                 xlims=(0,0), xshowaxis=false, 
+                 yshowaxis=false, label="", c=:viridis,
+                 colorbar_title=L"\min_\phi \gamma(\phi; A,f)", grid=false,framestyle=:white,background_color=:white,
+                 framecolor=:white,foreground_color_border=:white,
+                 foreground_color_axis=:white,colorbar_titlefontsize=5)
+
+# combine
+layout = @layout [grid(1,2) b{0.03w}]
+plt=plot(plt1,plt2,h2,layout=layout)
+
+# export
+plot!(size=(480,200))
+savefig("~/research/overleaf/rate_limited_sampling/figures/fig1_top.pdf")
+
+
+##################################################
+#%# Fig 2 bottom, line graph for uniform and optimal 
+freq_list = LinRange(3,4,nfreq);
+t     = LinRange(0,1,N+1)
+t     = t[1:end-1]
+nfreq=16
+pv= [0:.01:2*pi;]
+
+# aesthetic
+col1=[1,0,0];
+col2=[0,1,0];
+
+
+# left panel
+plt1=plot()
+for ii in [1:length(freq_list);]
+  fr=freq_list[ii]
+  f(phi) = getPower(t,phi,2,fr)
+  fv=map(f,pv)
+  plt1=plot!(pv,fv,lc=get_color_now(ii,length(freq_list),col1,col2))
+  
+  display(plt1)
+end
+plot!(ylims=(0,1),framecolor=:white,framestyle=:white)
+plot!(xlabel=L"$\phi$")
+plot!(ylabel=L"$\gamma(\phi;A,f)$")
+
+# right panel
+t=randn(length(t))
+plt2=plot()
+for ii in [1:length(freq_list);]
+  fr=freq_list[ii]
+  f(phi) = getPower(t,phi,2,fr)
+  fv=map(f,pv)
+  plt2=plot!(pv,fv,lc=get_color_now(ii,length(freq_list),col1,col2))
+  
+  display(plt2)
+end
+plot!(ylims=(0,1),framecolor=:white,framestyle=:white)
+plot!(xlabel=L"$\phi$")
+plot!(yticks=false)
+
+
+# dummy plot for colorbar
+h2=scatter([NaN NaN],[NaN NaN],zcolor=NaN,
+           xshowaxis=false, yshowaxis=false,
+           clims=(freq_list[1],freq_list[end]),
+           c=cgrad([RGB(col1[1],col1[2],col1[3]),
+           RGB(col2[1],col2[2],col2[3])],
+          [0,1]),colorbar=true,
+      location=:left,colorbar_titlefontsize=5,
+      colorbar_title=L"$f$")
+
+# combine
+layout = @layout [grid(1,2) b{0.03w}]
+plt=plot(plt1,plt2,h2,layout=layout)
+
+# export
+plot!(size=(480,200))
+savefig("~/research/overleaf/rate_limited_sampling/figures/fig1_bottom.pdf")
